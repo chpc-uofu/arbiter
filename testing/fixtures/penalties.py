b38da2cfc480421aa7d993a1044685fa594dd884
Kai-W-F <kai.w.forrest@gmail.com>
2024-11-13 22:26:19 +0000

    Update test fixtures

--- testing/fixtures/penalties.py
+++ testing/fixtures/penalties.py
@@ -4,19 +4,19 @@ from datetime import timedelta
 
 @pytest.fixture
 def harsh_penalty(db, harsh_limit_cpu, harsh_limit_mem):
-    return harsh_limit_cpu | harsh_limit_mem
+    return {'tiers':[harsh_limit_cpu | harsh_limit_mem]}
 
 
 @pytest.fixture
 def medium_penalty(db, medium_limit_cpu, medium_limit_mem):
-    return medium_limit_cpu | medium_limit_mem
+    return {'tiers': [medium_limit_cpu | medium_limit_mem]}
 
 
 @pytest.fixture
 def soft_penalty(db, soft_limit_cpu, soft_limit_mem):
-    return soft_limit_cpu | soft_limit_mem
+    return {'tiers': [soft_limit_cpu | soft_limit_mem]}
 
 
 @pytest.fixture
 def unset_penalty(db, unset_limit_cpu, unset_limit_mem):
-    return unset_limit_cpu | unset_limit_mem
+    return {'tiers': [unset_limit_cpu | unset_limit_mem]}
