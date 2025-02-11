353abf83f6468e8516be6b0093bccde43e39e260
Kai-W-F <kai.w.forrest@gmail.com>
2025-02-11 23:19:11 +0000

    Clean up code and render working dir in template

--- scripts/initialize.py
+++ scripts/initialize.py
@@ -1,20 +1,15 @@
 import shutil, os, sys
-from jinja2 import Template
+from jinja2 import Template, Environment, FileSystemLoader
 from importlib.resources import files
-import shutil
-import os
-import stat
-
-
 
 def initialize_config():
     interpreter_path = sys.executable
-    print(interpreter_path)
-
     current_path = os.getcwd()
-    arbiter_conf_dir = os.path.join(current_path, "arbiter")
+    config_template_dir = files("arbiter3.scripts.config_templates")
+    arbiter_conf_dir = os.path.join(current_path, "arbiter") 
     os.makedirs(arbiter_conf_dir, exist_ok=True)    
 
+    # Target file destinations
     created_manage = os.path.join(arbiter_conf_dir, "manage.py")
     created_settings = os.path.join(arbiter_conf_dir, "settings.py")
     created_base = os.path.join(arbiter_conf_dir, "base.py")
@@ -22,19 +17,23 @@ def initialize_config():
     created_web_service = os.path.join(arbiter_conf_dir, "arbiter-web.service")
     created_eval_service = os.path.join(arbiter_conf_dir, "arbiter-eval.service")
 
-    manage_template = str(files("arbiter3.scripts.config_templates").joinpath("manage.py"))
-    setting_template = str(files("arbiter3.scripts.config_templates").joinpath("settings.py"))
-    config_template = str(files("arbiter3.scripts.config_templates").joinpath("config.toml"))
-    base_template = str(files("arbiter3.scripts.config_templates").joinpath("base.py"))
 
+    #for whatever reason despite outputing the same type, the config template str does not output the path unless joined with ""
+    jinja_env = Environment(loader=FileSystemLoader(str(config_template_dir.joinpath(""))))
 
-    eval_service_template = Template(str(files("arbiter3.scripts.config_templates").joinpath("arbiter-eval.service.jinja")))
-    web_service_template = Template(str(files("arbiter3.scripts.config_templates").joinpath("arbiter-eval.service.jinja")))
+    # Config templates sources
+    manage_template = str(config_template_dir.joinpath("manage.py"))
+    setting_template = str(config_template_dir.joinpath("settings.py"))
+    config_template = str(config_template_dir.joinpath("config.toml"))
+    base_template = str(config_template_dir.joinpath("base.py"))
+    eval_service_template = jinja_env.get_template("arbiter-eval.service.jinja")
+    web_service_template = jinja_env.get_template("arbiter-web.service.jinja")
+
+    #Generate config files from source templates
 
     shutil.copy(manage_template, created_manage)
     os.chmod(created_manage, 0o744)
     print(f"Generated {created_manage}")
-    
 
     shutil.copy(setting_template, created_settings)
     print(f"Generated {created_settings}")
@@ -44,5 +43,13 @@ def initialize_config():
 
     shutil.copy(config_template, created_config)
     print(f"Generated {created_config}")
+
+    with open(created_eval_service, "w") as file:
+        file.write(eval_service_template.render(working_dir=str(arbiter_conf_dir)))
+        print(f"Generated {created_eval_service}")
+
+    with open(created_web_service, "w") as file:
+        file.write(web_service_template.render(working_dir=arbiter_conf_dir))
+        print(f"Generated {created_web_service}")
 
     
