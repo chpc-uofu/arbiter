5c3c3300397b5068c6e6d216b87c334c93d3a334
Kai-W-F <kai.w.forrest@gmail.com>
2025-02-28 19:18:50 +0000

    Quick search and initial breakdown

--- arbiter3/arbiter/views/user.py
+++ arbiter3/arbiter/views/user.py
@@ -1,26 +1,35 @@
-from django.forms.renderers import BaseRenderer
 from django.shortcuts import render, redirect
-from django import forms
-from django.utils.safestring import mark_safe
-from django.views.generic.list import ListView
-from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.decorators import login_required
-from django.urls import reverse_lazy
 from django.contrib.auth.decorators import permission_required
+from django.contrib import messages
 
 from arbiter3.arbiter.models import Target, Violation
 from .nav import navbar
 
+
 @permission_required('arbiter.view_dashboard')
 def get_user_lookup(request):
+    if request.method == "POST":
+        host = request.POST.get("host")
+        user = request.POST.get("user")
 
+        if not (host and user):
+            messages.error(request, "Both host and user required")
+        else:
+            target = Target.objects.filter(username=user, host=host).first()
+
+            if not target:
+                messages.error(request, "Specified host+user is not in arbiter's records (this may be because the names are incorrect or the user has not gone into penalty/base status before)")
+            else:
+                return redirect("arbiter:user-breakdown", target.id)
+    
     context = dict(
         title="User Lookup",
-        navbar=navbar(request)
+        hosts=set(Target.objects.values_list("host", flat=True)),
+        navbar=navbar(request),
     )
 
     return render(request, "arbiter/user_lookup.html", context)
+
 
 @permission_required('arbiter.view_dashboard')
 def get_user_breakdown(request, target_id):
@@ -30,7 +39,6 @@ def get_user_breakdown(request, target_id):
         messages.error(request, "Usage Policy not found.")
         return redirect("arbiter:list-usage-policy")
 
-    context = {"navbar": navbar(
-        request), "title": "Change Usage Policy"}
+    context = {"navbar": navbar(request), "target": target, "title": "User Breakdown"}
 
-    return render(request, "arbiter/change_view.html", context)
+    return render(request, "arbiter/user_breakdown.html", context)
