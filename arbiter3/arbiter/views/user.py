395da4463cfda2df89020df7efabecbc651dbfa1
Jackson Jay McKay <jackjaymckay@gmail.com>
2025-03-05 16:38:27 -0700

    39 target view (#43)
    * Reasonable max/min width settings so wide monitors dont look as ugly
    
    * hookup new views to functions (not implemented yet)
    
    * add user lookup to nav bar
    
    * don't show expire if base policy
    
    * Quick search and initial breakdown
    
    * add active limits and active violations
    
    * Add recent violation history
    
    * convert to readable limits
    
    * start moving lookup to just username
    
    * fixed typo
    
    * fix link styling
    
    * add link to user breakdown from violation
    
    * remove host box
    
    * Add info about where violation were triggered
    
    * convert to readable limits
    
    ---------
    
    Co-authored-by: Kai-W-F <kai.w.forrest@gmail.com>

--- arbiter3/arbiter/views/user.py
+++ arbiter3/arbiter/views/user.py
@@ -11,22 +11,17 @@ from .nav import navbar
 @permission_required('arbiter.view_dashboard')
 def get_user_lookup(request):
     if request.method == "POST":
-        host = request.POST.get("host")
         user = request.POST.get("user")
 
-        if not (host and user):
-            messages.error(request, "Both host and user required")
-        else:
-            target = Target.objects.filter(username=user, host=host).first()
+        targets = Target.objects.filter(username=user)
 
-            if not target:
-                messages.error(request, "Specified host+user is not in arbiter's records (this may be because the names are incorrect or the user has not gone into penalty/base status before)")
-            else:
-                return redirect("arbiter:user-breakdown", target.id)
+        if len(targets) == 0:
+            messages.error(request, "Specified user is not in arbiter's records (this may be because the names are incorrect or the user has not gone into penalty/base status before)")
+        else:
+            return redirect("arbiter:user-breakdown", user)
     
     context = dict(
         title="User Lookup",
-        hosts=set(Target.objects.values_list("host", flat=True)),
         navbar=navbar(request),
     )
 
@@ -34,19 +29,20 @@ def get_user_lookup(request):
 
 
 @permission_required('arbiter.view_dashboard')
-def get_user_breakdown(request, target_id):
-    target = Target.objects.filter(pk=target_id).first()
+def get_user_breakdown(request, username):
+    targets = Target.objects.filter(username=username).values("host", "limits")
 
-    if not target:
+    for target in targets:
+        target["limits"] = to_readable_limits(target["limits"])
+
+    if len(targets) == 0:
         messages.error(request, "Usage Policy not found.")
         return redirect("arbiter:list-usage-policy")
+
     
-    active_violations = Violation.objects.filter(target_id=target_id).exclude(expiration__lt = timezone.now())
-    recent_violations = Violation.objects.filter(target_id=target_id, expiration__lt = timezone.now()).order_by("-timestamp")[:10]
+    active_violations = Violation.objects.filter(target__username=username).exclude(expiration__lt = timezone.now())
+    recent_violations = Violation.objects.filter(target__username=username, expiration__isnull=False).order_by("-timestamp")[:10]
 
-    readable_limits = target.limits
-    readable_limits = to_readable_limits(readable_limits)
-
-    context = {"navbar": navbar(request),"title": "User Breakdown", "target": target, "limits":readable_limits, "active_violations": active_violations, "recent_violations": recent_violations}
+    context = {"navbar": navbar(request),"title": "User Breakdown", "username":username, "targets": targets, "active_violations": active_violations, "recent_violations": recent_violations}
 
     return render(request, "arbiter/user_breakdown.html", context)
