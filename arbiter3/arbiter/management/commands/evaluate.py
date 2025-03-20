88f4dc4f86bd386daafc182b2e648c0b239a9b7e
Kai-W-F <kai.w.forrest@gmail.com>
2025-03-20 20:36:20 +0000

    dont crash on concurrency error on eval

--- arbiter3/arbiter/management/commands/evaluate.py
+++ arbiter3/arbiter/management/commands/evaluate.py
@@ -1,9 +1,9 @@
 from django.core.management.base import BaseCommand
-from arbiter3.arbiter.eval import evaluate
+from arbiter3.arbiter.eval import evaluate, logger
 from django.utils import timezone
+from django.db.utils import OperationalError
 from arbiter3.arbiter.models import Policy
 from time import sleep
-
 
 class Command(BaseCommand):
     help = "Runs the entire arbiter service (Gets violations and applies penalties)"
@@ -24,12 +24,17 @@ class Command(BaseCommand):
         ).total_seconds()
 
         while True:
-            if options["policies"]:
-                policies = Policy.objects.filter(name__in=options["policies"])
-            else:
-                policies = Policy.objects.all()
+            try:
+                if options["policies"]:
+                    policies = Policy.objects.filter(name__in=options["policies"])
+                else:
+                    policies = Policy.objects.all()
 
-            evaluate(policies)
-            sleep(cycle_time)
-            if cycle_time == 0:
-                break
+                evaluate(policies)
+                #sleep(cycle_time)
+                if cycle_time == 0:
+                    break
+            except OperationalError:
+                logger.warning('evaluate loop failed to complete on operation error, likely an issue with concurency with sqlite locking the database')
+
+
